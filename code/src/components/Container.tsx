d7dc0c6804ef7bf0db6af5638d6cbc24284a6c8a
Huszka Adrián Gábor <hadrian@pollak.hu>
2025-12-25 15:52:29 +0100

    Update scroll

--- code/src/components/Container.tsx
+++ code/src/components/Container.tsx
@@ -2,20 +2,16 @@ import Stack from "@mui/material/Stack";
 import { styled } from "@mui/material/styles";
 
 export const Container = styled(Stack)(({ theme }) => ({
-    height: "calc((1 - var(--template-frame-height, 0)) * 100dvh)",
-    minHeight: "100%",
+    minHeight: "calc((1 - var(--template-frame-height, 0)) * 100dvh)",
+    position: "relative",
     padding: theme.spacing(2),
     [theme.breakpoints.up("sm")]: {
         padding: theme.spacing(4)
     },
-    "&::before": {
-        content: '""',
-        display: "block",
-        position: "absolute",
-        zIndex: -1,
-        inset: 0,
-        backgroundImage:
-            "radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))",
-        backgroundRepeat: "no-repeat"
-    }
+    backgroundColor: "hsl(220, 35%, 3%) !important",
+    backgroundImage:
+        "radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundAttachment: "fixed"
 }));
